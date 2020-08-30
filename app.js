fa804696ebd5311c4199dee344cc1615ce15cfab
anyahirota <anyahirota@gmail.com>
2020-08-30 15:56:40 -0700

    Prepare app for heroku deploy test 2

--- app.js
+++ app.js
@@ -24,5 +24,13 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
+const path = require("path");
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
